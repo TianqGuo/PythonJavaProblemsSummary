906030dd461c49a2876d8036331d5beb9c51eae3
Tianquan Guo <52896247+TianqGuo@users.noreply.github.com>
2020-11-23 22:31:55 -0800

    Update 4 problems

---
+++ MergeKLists.java
@@ -0,0 +1,42 @@
+package Test;
+import java.util.*;
+
+public class MergeKLists {
+	public ListNode merge(List<ListNode> listOfLists) {
+	    // Write your solution here/.
+	    PriorityQueue<ListNode> pq = new PriorityQueue<ListNode>(new Comparator<ListNode>() {
+	      @Override
+	      public int compare(ListNode e1, ListNode e2) {
+	        if (e1.value == e2.value) {
+	          return 0;
+	        }
+
+	        return e1.value < e2.value ? -1 : 1;
+	      }
+
+	    });
+
+	    ListNode dummy = new ListNode(-1);
+	    ListNode head = dummy;
+
+	    for (int i = 0; i < listOfLists.size(); i++) {
+	      if(listOfLists.get(i) != null) {
+	        pq.offer(listOfLists.get(i));
+	      }
+	    }
+
+	    while (!pq.isEmpty()) {
+	    	ListNode cur = pq.poll();
+	        dummy.next = cur;
+
+	        if (cur.next != null) {
+	          pq.offer(cur.next);
+	        }
+	        
+	        dummy = dummy.next;
+	    }
+
+	    return head.next;
+	  }
+
+}
---
+++ MergeSort.java
@@ -0,0 +1,65 @@
+package Test;
+
+public class MergeSort {
+
+  public int[] mergesort(int[] array) {
+    // Write your solution here
+    if (array == null || array.length <= 1) {
+      return array;
+    }
+    
+    return this.merge_recursion(array, 0, array.length -1);
+   
+  }
+
+  private int[] merge_recursion(int[] array, int left, int right) {
+	  
+	  if (left == right) {
+		  return new int[] {array[left]};
+	  }
+
+    int mid = left + (right -left)/2;
+    int[] left_merged = merge_recursion(array, left, mid);
+    int[] right_merged = merge_recursion(array, mid+1, right);
+
+    return this.merge_arrays(left_merged, right_merged);
+
+  }
+
+  private int[] merge_arrays(int[] left, int[] right) {
+
+    int i = 0;
+    int j = 0;
+    int cur = 0;
+    int[] merged = new int[left.length + right.length];
+
+    while (i < left.length && j < right.length) {
+      if (left[i] <= right[j]) {
+        merged[cur] = left[i];
+        cur++;
+        i++;
+      }
+
+      else {
+        merged[cur] = right[j];
+        cur++;
+        j++;
+      }
+    }
+
+    while (i < left.length) {
+      merged[cur] = left[i];
+      cur++;
+      i++;
+    } 
+
+    while (j < right.length) {
+      merged[cur] = right[j];
+      cur++;
+      j++;
+    }
+
+    return merged;
+  }
+
+}
